e388067eccbb2f597b71ca0f615f35134997ee3d
TonimatasDEV <amatascubero@gmail.com>
2023-03-30 21:26:42 +0200

    Start with ItemInit and PlayerInit

--- src/main/java/net/tonimatasdev/Main.java
+++ src/main/java/net/tonimatasdev/Main.java
@@ -2,23 +2,17 @@ package net.tonimatasdev;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.GameMode;
-import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.entity.Player;
-import net.minestom.server.entity.PlayerSkin;
 import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.PlayerBlockBreakEvent;
 import net.minestom.server.event.player.PlayerLoginEvent;
-import net.minestom.server.event.player.PlayerSkinInitEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.SharedInstance;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.item.ItemStack;
 import net.minestom.server.world.DimensionType;
+import net.tonimatasdev.init.ItemInit;
+import net.tonimatasdev.init.PlayerInit;
 import net.tonimatasdev.manager.Command;
-
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -33,29 +27,19 @@ public class Main {
             unit.modifier().fillHeight(62, 63, Block.GRASS_BLOCK);
         });
 
-        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
+        PlayerInit.register(eventHandler);
+        ItemInit.register(eventHandler);
+
 
         Command.register();
 
-        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+        eventHandler.addListener(PlayerLoginEvent.class, event -> {
             final Player player = event.getPlayer();
             event.setSpawningInstance(overworld);
             player.setRespawnPoint(new Pos(0, 64, 0));
 
             player.setGameMode(GameMode.CREATIVE);
-        });
-
-        globalEventHandler.addListener(PlayerSkinInitEvent.class, event -> {
-            PlayerSkin skin = PlayerSkin.fromUuid(event.getPlayer().getUuid().toString());
-            event.setSkin(skin);
-        });
-
-        globalEventHandler.addListener(PlayerBlockBreakEvent.class, event -> {
-            ItemEntity item = new ItemEntity(ItemStack.of(Objects.requireNonNull(event.getBlock().registry().material())));
-            item.setPickable(true);
-            item.setVelocity(new Vec(0, 5.5, 0));
-            item.setInstance(event.getInstance(), new Pos(event.getBlockPosition().x() + 0.5, event.getBlockPosition().y() + 0.5, event.getBlockPosition().z() + 0.5));
-
         });
 
         minecraftServer.start("0.0.0.0", 25565);
---
+++ src/main/java/net/tonimatasdev/init/PlayerInit.java
@@ -0,0 +1,27 @@
+package net.tonimatasdev.init;
+
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.entity.GameMode;
+import net.minestom.server.entity.ItemEntity;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerBlockBreakEvent;
+import net.minestom.server.item.ItemStack;
+
+import java.util.Objects;
+
+public class PlayerInit {
+    public static void register(GlobalEventHandler eventHandler) {
+        eventHandler.addListener(PlayerBlockBreakEvent.class, event -> {
+            if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
+                ItemEntity item = new ItemEntity(ItemStack.of(Objects.requireNonNull(event.getBlock().registry().material())));
+                item.setPickable(true);
+                item.setVelocity(new Vec(0, 5.5, 0));
+                item.setPose(event.getPlayer().getPose());
+                item.setInstance(event.getInstance(), new Pos(event.getBlockPosition().x() + 0.5, event.getBlockPosition().y() + 0.5, event.getBlockPosition().z() + 0.5));
+            }
+        });
+    }
+
+
+}
