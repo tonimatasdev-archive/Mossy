b405b2d5d054a04d02a2a5b28ffa8cfa2f5eae13
TonimatasDEV <amatascubero@gmail.com>
2023-04-04 23:50:57 +0200

    Fix player spawn in air and improve block drop

--- src/main/java/net/tonimatasdev/Main.java
+++ src/main/java/net/tonimatasdev/Main.java
@@ -1,13 +1,8 @@
 package net.tonimatasdev;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.GameMode;
-import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.instance.InstanceContainer;
-import net.minestom.server.instance.SharedInstance;
 import net.minestom.server.world.DimensionType;
 import net.tonimatasdev.generator.NoiseGenerator;
 import net.tonimatasdev.impl.BlockImpl;
@@ -16,29 +11,21 @@ import net.tonimatasdev.impl.PlayerImpl;
 import net.tonimatasdev.manager.Command;
 
 public class Main {
+    public static InstanceContainer overWorld;
+
     public static void main(String[] args) {
         MinecraftServer minecraftServer = MinecraftServer.init();
-        InstanceContainer overworld = MinecraftServer.getInstanceManager().createInstanceContainer(DimensionType.OVERWORLD);
-        SharedInstance sharedInstance = MinecraftServer.getInstanceManager().createSharedInstance(overworld);
 
-        //instanceContainer.setGenerator(new NoiseGenerator());
-        overworld.setGenerator(new NoiseGenerator());
+        overWorld = MinecraftServer.getInstanceManager().createInstanceContainer(DimensionType.OVERWORLD);
+
+        overWorld.setGenerator(new NoiseGenerator());
 
         GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
         BlockImpl.register(eventHandler);
         ItemImpl.register(eventHandler);
         PlayerImpl.register(eventHandler);
 
-
         Command.register();
-
-        eventHandler.addListener(PlayerLoginEvent.class, event -> {
-            final Player player = event.getPlayer();
-            event.setSpawningInstance(overworld);
-            player.setRespawnPoint(new Pos(0, 100, 0));
-
-            player.setGameMode(GameMode.CREATIVE);
-        });
 
         minecraftServer.start("0.0.0.0", 25565);
     }
--- src/main/java/net/tonimatasdev/impl/PlayerImpl.java
+++ src/main/java/net/tonimatasdev/impl/PlayerImpl.java
@@ -6,17 +6,40 @@ import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerBlockBreakEvent;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.ItemStack;
+import net.tonimatasdev.Main;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class PlayerImpl {
+    @SuppressWarnings("UnstableApiUsage")
     public static void register(GlobalEventHandler eventHandler) {
+        eventHandler.addListener(PlayerLoginEvent.class, event -> {
+            event.setSpawningInstance(Main.overWorld);
+
+            event.getPlayer().setGameMode(GameMode.SURVIVAL);
+            Main.overWorld.loadChunk(0, 0).join();
+
+            int y = Main.overWorld.getDimensionType().getMaxY();
+
+            while (Block.AIR.compare(Main.overWorld.getBlock(0, y, 0))) {
+                y--;
+                if (y == Main.overWorld.getDimensionType().getMinY()) {
+                    break;
+                }
+            }
+            event.getPlayer().setRespawnPoint(new Pos(0, y, 0));
+        });
+
         eventHandler.addListener(PlayerBlockBreakEvent.class, event -> {
             if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
                 ItemEntity item = new ItemEntity(ItemStack.of(Objects.requireNonNull(event.getBlock().registry().material())));
                 item.setPickable(true);
-                item.setVelocity(new Vec(0, 5.5, 0));
+                Random random = new Random();
+                item.setVelocity(new Vec(random.nextDouble() * (2.5 - (-2.5)) + (-2.5), 3.9, random.nextDouble() * (2.5 - (-2.5)) + (-2.5)));
                 item.setPose(event.getPlayer().getPose());
                 item.setInstance(event.getInstance(), new Pos(event.getBlockPosition().x() + 0.5, event.getBlockPosition().y() + 0.5, event.getBlockPosition().z() + 0.5));
             }
