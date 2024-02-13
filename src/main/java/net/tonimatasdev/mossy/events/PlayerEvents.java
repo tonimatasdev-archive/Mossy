b6225232866b6b466e662c687ed44ce7433a2509
TonimatasDEV <amatascubero@gmail.com>
2024-02-13 22:58:16 +0100

    Fix build

--- src/main/java/net/tonimatasdev/mossy/events/PlayerEvents.java
+++ src/main/java/net/tonimatasdev/mossy/events/PlayerEvents.java
@@ -1,23 +1,16 @@
 package net.tonimatasdev.mossy.events;
 
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.GameMode;
-import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.PlayerBlockBreakEvent;
-import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.item.ItemStack;
 import net.tonimatasdev.mossy.Mossy;
-
-import java.util.Objects;
-import java.util.Random;
 
 public class PlayerEvents {
     @SuppressWarnings("UnstableApiUsage")
     public static void init(GlobalEventHandler eventHandler) {
-        eventHandler.addListener(PlayerLoginEvent.class, event -> {
+        eventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             event.setSpawningInstance(Mossy.overWorld);
 
             event.getPlayer().setGameMode(GameMode.SURVIVAL);
@@ -32,19 +25,6 @@ public class PlayerEvents {
                 }
             }
             event.getPlayer().setRespawnPoint(new Pos(0, y, 0));
-        });
-
-        eventHandler.addListener(PlayerBlockBreakEvent.class, event -> {
-            if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
-                ItemEntity item = new ItemEntity(ItemStack.of(Objects.requireNonNull(event.getBlock().registry().material())));
-                item.setPickable(true);
-                item.setMergeable(true);
-                item.setMergeRange(1.6f);
-                Random random = new Random();
-                item.setVelocity(new Vec(random.nextDouble() * (2.2 - (-2.2)) + (-2.2), 3.6, random.nextDouble() * (2.2 - (-2.2)) + (-2.2)));
-                item.setPose(event.getPlayer().getPose());
-                item.setInstance(event.getInstance(), new Pos(event.getBlockPosition().x() + 0.5, event.getBlockPosition().y() + 0.5, event.getBlockPosition().z() + 0.5));
-            }
         });
     }
 }
